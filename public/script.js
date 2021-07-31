5c38c77a69bf30a6503a32ef513fb1bbb0c11bf7
Your Name <you@example.com>
2021-07-31 15:31:48 +0530

    initial

---
+++ public/script.js
@@ -0,0 +1,12 @@
+const socket = io('/')
+
+// const myPeer = new Peer(undefined, {
+//     host: '/',
+//     port: '3001'
+//   })
+
+socket.emit('join-room', ROOM_ID, 10)
+
+socket.on('user-conneted', userId => {
+    console.log('User connected: ' + userId)
+})
